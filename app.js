6a7de3219a6584354b135420ad08bdede152b7f5
Ashu9l <ashu8795270396@gmail.com>
2025-04-03 02:33:14 +0530

    update mongo Db uri and env

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const {
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-mongoose.connect( process.env.MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect( process.env.MONGO_URL , { useNewUrlParser: true, useUnifiedTopology: true })
 
  .then((e) => console.log("MongoDB Connected"));
 
